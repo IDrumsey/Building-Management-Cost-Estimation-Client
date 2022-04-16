3ac079abb8f32426ee251e94fdadfd56977c67ae
IDrumsey <clayofcasper@gmail.com>
2022-04-16 13:33:08 -0400

    add project.

--- src/app/core/components/location-card/location-card.component.ts
+++ src/app/core/components/location-card/location-card.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { faCancel, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { LocationSummary } from '../../data/models/location-summary';
+import { Project } from '../../data/models/project';
 
 @Component({
   selector: 'app-location-card',
@@ -17,16 +20,44 @@ export class LocationCardComponent implements OnInit {
 
   @Input() location: LocationSummary
 
+  newProjectTitle = new FormControl('', Validators.required)
+  @ViewChild('newProjectTitleEl') set newProjectTitleEl(el: ElementRef) {
+    if(el){
+      el.nativeElement.focus()
+    }
+  }
+
   // ------------------------------------ STATE ------------------------------------
 
   showingProjects = false
+  addingNewProject = false
 
   // ------------------------------------ EVENTS ------------------------------------
+
+  // ------------------------------------ STYLES ------------------------------------
+
+  checkIcon = faCheck
+  cancelIcon = faCancel
 
   // ------------------------------------ EVENT HANDLERS ------------------------------------
 
   onHeaderClick(): void {
     this.toggleShowingDetails()
+  }
+
+  onAddProjectBtnClick(): void {
+    this.addingNewProject = true
+  }
+
+  onNewProjectCancel(): void {
+    this.addingNewProject = false
+  }
+
+  onNewProjectConfirm(): void {
+    const newProject = new Project()
+    newProject.title = this.newProjectTitle.value
+    this.location.projects.push(newProject)
+    this.addingNewProject = false
   }
 
   // ------------------------------------ FUNCTIONALITY ------------------------------------
